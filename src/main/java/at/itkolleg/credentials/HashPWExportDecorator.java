bbf036bda09c7a152dca669198ae100b290abbb1
beatlesally <sevoetter@tsn.at>
2022-11-16 08:33:42 +0100

    implemented method export

--- src/main/java/at/itkolleg/credentials/HashPWExportDecorator.java
+++ src/main/java/at/itkolleg/credentials/HashPWExportDecorator.java
@@ -1,4 +1,22 @@
 package at.itkolleg.credentials;
 
-public class HashPWExportDecorator {
+import com.google.common.hash.Hashing;
+
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
+public class HashPWExportDecorator extends DecoratorExport{
+    public HashPWExportDecorator(ExportCredentials ex) {
+        super(ex);
+    }
+
+    @Override
+    public void export(List<Credentials> credentialsList) {
+        for (Credentials credentials:credentialsList) {
+            credentials.setPwd(Hashing.sha256()
+                    .hashString(credentials.getPwd(), StandardCharsets.UTF_8)
+                    .toString());
+        }
+        super.export(credentialsList);
+    }
 }
